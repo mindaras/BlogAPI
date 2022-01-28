e5a97b47c04d482292282ff67276954ab2b8d155
Mindaugas Lazauskas <mindaugaslazauskas@Rugiles-MacBook-Pro.local>
2022-01-28 23:00:10 +0200

    added cookie-parser middleware

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import cors from "cors";
 import express from "express";
 import { postsApi } from "./modules/posts/api";
 import { usersApi } from "./modules/users/api";
+import cookieParser from "cookie-parser";
 
 const init = async () => {
   const app = express();
 
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
   app.use("/api/users", usersApi);
   app.use("/api/posts", postsApi);
   app.listen(process.env.PORT, () =>
